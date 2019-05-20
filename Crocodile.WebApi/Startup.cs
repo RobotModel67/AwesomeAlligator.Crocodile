59fa4cb2bd410eaa329d90e4862b32d8435deab4
WarMachineEngine <WarMachineEngine@gmail.com>
2019-05-20 15:56:17 -0500

    Angular SPA

--- Crocodile.WebApi/Startup.cs
+++ Crocodile.WebApi/Startup.cs
@@ -29,6 +29,7 @@ namespace Crocodile.WebApi
         {
             services.AddDbContext<Repository>(x => x.UseMySQL(Configuration.GetConnectionString("MySqlConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace Crocodile.WebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
